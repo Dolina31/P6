5d412214935f2a3e859aacde7f4b1b26aa7026aa
Line Ibo <ibo.line@hotmail.fr>
2023-05-15 17:01:53 +0200

    ajout des travaux via le DOM, création des filtres pour le moment non fonctionnel après plusieurs essaies qui n'on pas marché. Début du travail sur la page de login

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -5,10 +5,30 @@ let works = [];
 
 
 async function WorksImport() {
+
     await fetch("http://localhost:5678/api/works")
         .then((res) => res.json())
         .then((data) => (works = data));
 
-    console.log(works);
+
+    works.forEach((work) => {
+        const figure = document.createElement("figure");
+        gallery.appendChild(figure);
+        figure.classList = work.category.name;
+
+        const img = document.createElement("img");
+        img.src = work.imageUrl;
+        img.alt = work.title;
+        figure.appendChild(img);
+
+        const figcaption = document.createElement("figcaption");
+        figcaption.innerHTML = work.title;
+        figure.appendChild(figcaption);
+
+        console.log(work.category);
+    })
+
 }
+WorksImport()
+
 
